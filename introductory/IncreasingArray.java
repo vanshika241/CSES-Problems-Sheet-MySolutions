49ba7015f0fb52541a29ef39008ca8787a6f6a64
codeVibes:) <123478862+vanshika241@users.noreply.github.com>
2026-09-20 20:21:33 +0530

    Update IncreasingArray.java

--- introductory/IncreasingArray.java
+++ introductory/IncreasingArray.java
@@ -1,40 +1,30 @@
 package introductory;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class IncreasingArray {
-    static int findMissing(int[] arr, int n) {
-        int len = n - 1;
-        int i = 0;
-        int moves = 0;
-        while (i < len) {
-            int correctIdx = arr[i] - 1;
-            if (correctIdx >= 0 && correctIdx < len && arr[i] != arr[correctIdx]) {
-                int temp = arr[correctIdx];
-                arr[correctIdx] = arr[i];
-                arr[i] = temp;
-            } else {
-                i++;
-            }
-        }
-        return n;
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine().trim());
-        if (n == 1) {
-            System.out.println(1);
-            return;
-        }
-        int[] arr = new int[n - 1];
+        int[] arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n - 1; i++) {
+        for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        System.out.println(findMissing(arr, n));
+        System.out.println(findMovestoMakeIncreasing(arr , n));
     }
+
+    private static long findMovestoMakeIncreasing(int[] arr, int n) {
+        long moves = 0;
+        for(int i=1;i<n;i++){
+            if(arr[i-1] > arr[i]){
+                moves += (arr[i-1] - arr[i]);
+                arr[i] = arr[i-1];
+            }
+        }
+        return  moves;
     }
+}
+
